993c054effd0b4b5781472acdb18feac9eae655e
Daniel Xavier <danieelxavier@gmail.com>
2019-01-10 09:37:28 -0400

    update script runFirst

--- runFirstLevel.sh
+++ runFirstLevel.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rm result.txt
+rm resultFirstLevel.txt
 
 echo "----------------------------------------------------------------------------------------------"
 echo "TWO LEVEL"
@@ -13,26 +13,26 @@ make
 for((j=4;j<=10;j=j+1))
 do 
 
-    echo -e "----------------------------------------------------------------------------------------------\n    $j CARACTERES\n----------------------------------------------------------------------------------------------" >> ../result.txt
+    echo -e "----------------------------------------------------------------------------------------------\n    $j CARACTERES\n----------------------------------------------------------------------------------------------" >> ../resultFirstLevel.txt
 
     for((i=3;i<=13;i=i+2))
     do 
         echo "two-level - usaddr - q$i - tau:1 - index1: $j"
-        ./twoltrie ~/usaddr/usaddr.txt ~/usaddr/q$i.txt 1 $j >> ../result.txt
+        ./twoltrie ~/usaddr/usaddr.txt ~/usaddr/q$i.txt 1 $j >> ../resultFirstLevel.txt
     done
 
 
-    for((i=3;i<=13;i=i+2))
+    for((i=5;i<=13;i=i+2))
     do 
         echo "two-level - usaddr - q$i - tau:2 - index1: $j"
-        ./twoltrie ~/usaddr/usaddr.txt ~/usaddr/q${i}_10.txt 2 $j >> ../result.txt
+        ./twoltrie ~/usaddr/usaddr.txt ~/usaddr/q${i}_10.txt 2 $j >> ../resultFirstLevel.txt
     done
 
 
-    for((i=3;i<=13;i=i+2))
+    for((i=5;i<=13;i=i+2))
     do 
         echo "two-level - usaddr - q$i - tau:3 - index1: $j"
-        ./twoltrie ~/usaddr/usaddr.txt ~/usaddr/q${i}_10.txt 3 $j >> ../result.txt
+        ./twoltrie ~/usaddr/usaddr.txt ~/usaddr/q${i}_10.txt 3 $j >> ../resultFirstLevel.txt
     done
     
 done
